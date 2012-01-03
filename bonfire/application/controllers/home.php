9ad83d97e4f284ea2a732229857032c5fdbd5493
Rona Dini Hari <rona.dinihari@gmail.com>
2012-01-03 11:01:21 +0700

    Points to /content/media from home site after login.

--- bonfire/application/controllers/home.php
+++ bonfire/application/controllers/home.php
@@ -5,7 +5,17 @@ class Home extends Front_Controller {
 	//--------------------------------------------------------------------
 	
 	public function index() 
-	{	
+	{
+		parent::__construct();
+
+		// acessing our userdata cookie
+		$cookie = unserialize($this->input->cookie($this->config->item('sess_cookie_name')));
+		$logged_in = isset ($cookie['logged_in']);
+	
+		if ($logged_in) {
+			Template::redirect(SITE_AREA .'/content/media');
+		}
+		
 		Template::render();
 	}
 	
